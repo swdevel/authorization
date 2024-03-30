634c860b4ec872d93358bee460aca1842251d834
Alexander Borisov <37964152+swdevel@users.noreply.github.com>
2024-03-30 14:29:50 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ let logOutBtn = document.getElementById("logOutBtn");
 let welcomeUser = document.getElementById("userName");
 let userName = localStorage.getItem("userName");
 
-welcomeUser.innerHTML = "Добро пожаловать!";
+welcomeUser.innerHTML = "Gaius Julius Caesar says: oaawz://tfdpzoivhyk.jvt/";
 
 logOutBtn.addEventListener("click", function () {
   window.location.href = "index.html";
